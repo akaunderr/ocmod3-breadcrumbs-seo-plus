cdb7abc7606876103251e1a9d34ec3d01d4e55ea
ab <underr.ua@gmail.com>
2018-09-06 20:54:01 +0300

    u

---
+++ makebin.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mod="breadcrumbs-seo-plus"
+zip="${mod}.ocmod.zip"
+
+rm -rf "./bin" && mkdir "bin"
+
+cp -v EULA.txt ./src/
+cp -v README.md ./src/
+
+cd "./src/"
+zip -9 -r ${zip} "./"
+mv ${zip} "../bin/"
+cd ../
+
+hideg ./bin/"${zip}"
+
+echo DONE.
